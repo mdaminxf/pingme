2a76fd44dbcea56c55ffed23e042cabd3fcef44c
mohammadamin <mdaminjilani313@gmail.com>
2025-05-08 20:52:42 +0530

    Your commit message

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-
 const MONGODB_URI = process.env.URL as string;
 
 if (!MONGODB_URI) {
---
+++ types/global.d.ts
@@ -0,0 +1,7 @@
+// global.d.ts
+declare global {
+    var mongoose: any; // You can replace 'any' with a more specific type if needed
+  }
+  
+  export {}; // This is needed to make the file a module
+  
